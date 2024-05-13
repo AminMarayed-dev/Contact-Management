62856658c20bf6159312144fb06f648d040d09c6
AminMarayed <aminmarayed4@gmail.com>
2024-05-13 15:13:53 +0330

    feat: add form and list-users inside App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-
-
+import Form from "./components/form/Form";
+import ListUsers from "./components/list-users/ListUsers";
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <div className="flex px-3 mt-4 gap-4">
+      <Form />
+      <ListUsers />
+    </div>
+  );
 }
 
-export default App
+export default App;
